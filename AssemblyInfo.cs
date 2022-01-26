ecf54e5a46640ad7a80b91fc4eabcca9b39b9f79
Erling Jahr <erling@fintlabs.no>
2022-01-26 12:31:57 +0100

    updated to prod version

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -43,9 +43,9 @@ using System.Runtime.CompilerServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyInformationalVersion("1.1.0")]
-[assembly: AssemblyVersion("1.4.2.*")]
-
+//[assembly: AssemblyInformationalVersion("1.1.0")]
+//[assembly: AssemblyVersion("1.4.2.*")]
+[assembly: AssemblyFileVersion("1.7.0")]
 //
 // In order to sign your assembly you must specify a key to use. Refer to the
 // Microsoft .NET Framework documentation for more information on assembly signing.
